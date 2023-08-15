b00e15f5727cb16673418fa06be7e88f862e1bee
oum-s <oumiyouss@hotmail.fr>
2023-08-16 01:51:51 +0200

    modif css

--- src/components/Pages/Experience/Experience.js
+++ src/components/Pages/Experience/Experience.js
@@ -1,27 +1,26 @@
 import './Experience.scss';
-import projet from './../../../assets/cdp.png';
-import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+import projetTL from './../../../assets/project__teenlabs.png';
+import projetOC from '../../../assets/project__kasaa.jpg';
+import projetSkyblog from '../../../assets/project__skyblog.jpg';
+import projetOC2 from '../../../assets/project__bookifr.png';
+import logoOC from '../../../assets/OC_logo.png';
+import logoTL from '../../../assets/teenlabs-logo.jpg';
+import logoSky from '../../../assets/skyblog--logo.png';
+
+
 
 
 
 function Experience() {
-  let experienceCard = document.getElementsByClassName('experience__project__card')
-
-  const [isActive, setIsActive] = useState(false);
   
-  console.log(isActive, 'active')
-  
-  /* const handleClick = (e) => {
-    setIsActive(!isActive)
-    experienceCard.add('activeDiv')
-    console.log("clicked")
-  } */
   return (
     <section id="Experience" className='container'>
       <h3 className="special"> Mes projets </h3>
 
         <div className='experience'>
-          <div className='experience__descr'>
+          <div className='experience__descr' >
             <div className='experience__descr__row'>
               <div className='experience__descr__row__date'>
                 <p>2022</p>
@@ -58,13 +57,18 @@ function Experience() {
               <div className='experience__project__cards__ctn--1'>
                 <div className='experience__project__cards__ctn--1__row'>
                   <div 
-                  className="experience__project__card up"
-                  /* id={isActive ? 'activeDiv' : 'desaDiv'} */
-                  /* onClick={handleClick} */>
-                  {/* <img src={projet} alt='projet' /> */}
+                  className="experience__project__card up">
+                    <img src={projetTL} alt='projet' /> 
                   </div>
                   <div className='hidden__card  up'>
-                    <h1>I M HERE GUYSSSS</h1>
+                    <div className='hidden__card__img'>
+                      <img src={logoTL} alt='teenlabs__logo'></img>
+                    </div>
+                    <h5>Teenlabs</h5>
+                    <p>Langages utilisés: PHP/MySQ</p>
+                    <a href="/">
+                      <FontAwesomeIcon className='faLink' icon={faLink} />
+                    </a>
                   </div>
                 </div>
                 
@@ -74,13 +78,18 @@ function Experience() {
 
                 <div className='experience__project__cards__ctn--2__column--1'>
                       <div 
-                      className="experience__project__card alone"
-                      /* id={isActive ? 'activeDiv' : 'desaDiv'} */
-                      /* onClick={handleClick} */>
-                      {/* <img src={projet} alt='projet' /> */}
+                      className="experience__project__card alone">
+                      <img src={projetSkyblog} alt='skyblog' /> 
                       </div>
                       <div className='hidden__card alone'>
-                      <h1>I M HERE GUYSSSS</h1>
+                        <div className='hidden__card__img'>
+                          <img src={logoSky} alt='skyblog__logo'></img>
+                        </div>
+                        <h5>Skyblog</h5>
+                        <p>Langages utilisés: HTML/CSS</p>
+                        <a href="https://github.com/oum-s/skyblog/deployments/github-pages">
+                          <FontAwesomeIcon className='faLink' icon={faLink} />
+                        </a>
                       </div>
                 </div>
 
@@ -88,25 +97,35 @@ function Experience() {
 
                   <div className='hideAndSeek'>
                     <div 
-                    className="experience__project__card clmn first"
-                    /* id={isActive ? 'activeDiv' : 'desaDiv'} */
-                    /* onClick={handleClick} */>
-                    {/* <img src={projet} alt='projet' /> */}
+                    className="experience__project__card clmn first">
+                    <img src={projetOC} alt='projet__oc' /> 
                     </div>
                     <div className='hidden__card first'>
-                    <h1>I M HERE GUYSSSS</h1>
-                  </div>
+                      <div className='hidden__card__img oc'>
+                        <img src={logoOC} alt='oopenclassrooms__logo'></img>
+                      </div>
+                      <h5>OPENCLASSROOMS</h5>
+                      <p>Langage utilisé: React</p>
+                      <a href="https://github.com/oum-s/P5-Kanap">
+                          <FontAwesomeIcon className='faLink' icon={faLink} />
+                      </a>
+                    </div>
                   </div>
                   <div className='hideAndSeek'>
                     <div 
-                    className="experience__project__card clmn second"
-                    /* id={isActive ? 'activeDiv' : 'desaDiv'} */
-                    /* onClick={handleClick} */>
-                    {/* <img src={projet} alt='projet' /> */}
+                    className="experience__project__card clmn second">
+                    <img src={projetOC2} alt='projet__oc' /> 
                     </div>
                     <div className='hidden__card second'>
-                    <h1>I M HERE GUYSSSS</h1>
-                  </div>
+                      <div className='hidden__card__img oc'>
+                        <img src={logoOC} alt='openclassrooms__logo'></img>
+                      </div>
+                      <h5>OPENCLASSROOMS</h5>
+                      <p>Langage utilisé: HTML/SCSS</p>
+                      <a href="https://github.com/oum-s/P2-BookiFr/deployments/github-pages">
+                          <FontAwesomeIcon className='faLink' icon={faLink} />
+                      </a>
+                    </div>
                   </div>
 
                 </div>
